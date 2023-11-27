90aac78f7c0f7481a337331140f5f839127e41f9
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-11-27 23:59:46 +0300

    Fix an error in the users controller

--- app/controllers/v1/api/users_controller.rb
+++ app/controllers/v1/api/users_controller.rb
@@ -2,7 +2,7 @@ class V1::Api::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user, status: created
+            render json: @user, status: :created
         else
             render json: @user.errors, status: :unprocessable_entity
         end
